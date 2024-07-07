b5844fb9a4f5f7f75d7fa8d7355b6491bf1c4bb4
Dinorush <onparnorthstar@gmail.com>
2024-07-06 20:32:13 -0400

    Add project files.

---
+++ FullAutoEverything/WeaponPatch.cs
@@ -0,0 +1,18 @@
+﻿using Gear;
+using HarmonyLib;
+
+namespace ColorCrosshair
+{
+    [HarmonyPatch]
+    internal static class WeaponPatch
+    {
+        [HarmonyPatch(typeof(BulletWeapon), nameof(BulletWeapon.SetupArchetype))]
+        [HarmonyPostfix]
+        [HarmonyWrapSafe]
+        public static void Post_SetupArchetype(BulletWeapon __instance)
+        {
+            if (__instance.m_archeType != null)
+                __instance.m_archeType.m_triggerNeedsPress = false;
+        }
+    }
+}
